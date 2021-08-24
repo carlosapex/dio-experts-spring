95ed9aadb86bcf541ad0111452bb28fe8b7a2a98
carlosapex <85699361+carlosapex@users.noreply.github.com>
2021-08-24 15:22:43 -0300

    Add files via upload

---
+++ shopping-cart/src/main/java/digitalinnovation/one/experts/shoppingcart/ShoppingCartApplication.java
@@ -0,0 +1,17 @@
+package digitalinnovation.one.experts.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//@EnableEurekaClient
+//@EnableDiscoveryClient
+public class ShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartApplication.class, args);
+	}
+
+}
